ee22e29e0da0c42b65d7cc274d5b72181ae11701
brawat10 <bijayendra10@gmail.com>
2025-09-20 22:17:51 +1000

    Modifies Config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['json', {outputFile: 'results.json'}]],
+  reporter: 'html',
+
+  // reporter: [['json', {outputFile: 'results.json'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+     
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://restful-booker.herokuapp.com',
@@ -43,7 +46,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    }
 
     // Using Global.setup
     // {
